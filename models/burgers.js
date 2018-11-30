68692471ac9630c8fa167973ffd58c0812d40a63
Chan Lee <chano@Chans-MacBook-Pro.local>
2018-11-29 22:10:48 -0500

    chan

---
+++ models/burgers.js
@@ -0,0 +1,19 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  update: function(id, cb) {
+    orm.update("burgers", id, cb);
+  },
+  create: function(name,cb){
+    orm.create("burgers", name, cb);
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
